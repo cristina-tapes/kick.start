0dbdd101f93d3cfd9028da6573604b307730a2de
Cristina Tapes <cristina.tapes@itiviti.com>
2019-03-05 01:07:54 +0200

    Add dummy test and code in order the test to pass

--- EvenDigits/src/kick/start/EvenDigits.java
+++ EvenDigits/src/kick/start/EvenDigits.java
@@ -5,4 +5,8 @@ public class EvenDigits {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static long GetMinimumNumberOfStepsUntilAllDigitsEven(long number){
+        return 0;
+    }
 }
--- EvenDigits/src/tests/EvenDigitsTest.java
+++ EvenDigits/src/tests/EvenDigitsTest.java
@@ -1,7 +1,21 @@
 package tests;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import kick.start.EvenDigits;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(JUnitParamsRunner.class)
 class EvenDigitsTest {
 
+    @Test
+    public void GivenTwo_GetMinimumNumberOfStepsUntilAllDigitsEven_ReturnsZero(){
+        long actual=EvenDigits.GetMinimumNumberOfStepsUntilAllDigitsEven(2);
+        long expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
 }
